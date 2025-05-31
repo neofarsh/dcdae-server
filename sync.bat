f26e2dfce563d67560db6f8f3dbed5b3b4648f88
Your Name <you@example.com>
2025-05-31 13:08:57 +0300

    ╨░╨▓╤В╨╛-╤Б╨╕╨╜╤Е╤А╨╛╨╜╨╕╨╖╨░╤Ж╨╕╤П

--- sync.bat
+++ sync.bat
@@ -1,7 +1,12 @@
 @echo off
 setlocal
 
+:: Пути к файлам
+set "SOURCE=C:\Users\neofarsh\AppData\Roaming\nLauncher\dcdae-server-golden-wind\xaero\minimap\Multiplayer_d2.rustix.me\dim%%0\mw$-1435029187_2.txt"
+set "DEST=C:\Users\neofarsh\Desktop\dcdae-server\maps\mw$-1435029187_2.txt"
 
+:: Копируем файл с заменой
+copy /Y "%SOURCE%" "%DEST%"
 
 :: Выполняем git-команды
 git add .
